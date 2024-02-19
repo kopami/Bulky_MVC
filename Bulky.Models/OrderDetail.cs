e9d8452c5d0f8993af9dec577961d67a8ccdd9e4
kopami <khoipham2310@gmail.com>
2024-02-20 00:12:12 +0700

    feat: Create Order Header and Order Detail Model

---
+++ Bulky.Models/OrderDetail.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; } = null!;
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; } = null!;
+        public int Count { get; set; }
+        public double Price { get; set; } 
+        // Price at the time of purchase
+        //Price of Product may change in future but this Price will remain same
+                                            
+        
+        
+    }
+}
